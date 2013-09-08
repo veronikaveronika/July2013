f69353e4823afc4e68b5e397f61038e4e0022455
Veronika <veronika.trunina@gmail.com>
2013-09-08 13:35:52 +0400

    Age and full_names are added to children and parents lists

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,6 +1,14 @@
 class Parent < ActiveRecord::Base
   belongs_to :family
   validates_inclusion_of :sex, in: %w( m f )
+  validates_presence_of :dob
 
   def full_name; "#{last_name} #{first_name} #{middle_name}" end
+  def age
+    unless self.dob.nil?
+      now = Time.now.utc.to_date
+      now.year - self.dob.year - (self.dob.to_date.change(:year => now.year) > now ? 1 : 0)
+      end
+	end
+  
 end
